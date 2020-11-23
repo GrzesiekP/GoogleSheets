c59f54007e23bff652ad1b66b74e53f884098f8e
Grzesiek P <kontakt@grzegorzpawlowski.pl>
2020-11-23 18:33:31 +0100

    dodaj kategorie funduszy

--- nnFundPrice.js
+++ nnFundPrice.js
@@ -1,20 +1,35 @@
-function FUND_VALUE(fundName) {
-    let url = "https://www.nntfi.pl/fundusze-inwestycyjne/fundusze-akcji/" + fundName + "?unitsCategoryId=A"
-    let options =
-        {
-          "method"  : "GET",
-          "followRedirects" : true,
-          "muteHttpExceptions":true
-        };
-    let result = UrlFetchApp.fetch(url, options).getContentText();
-    
-    const pattern = '<div class="end_date_price"><span class="fund_value">'
-    let startIndex = result.lastIndexOf(pattern) + pattern.length
-    let newString = result.substring(startIndex)
-    let endIndex = newString.indexOf(' <')
-    let priceString = newString.substr(0, endIndex).replace(',','.')
-    
-    let price = parseFloat(priceString)
-    
-    return price
+function FUND_VALUE(fundType, fundName) {
+  fundType = fundType.trim()
+  fundName = fundName.trim()
+  let url = "https://www.nntfi.pl/fundusze-inwestycyjne/" + fundType + "/" + fundName + "?unitsCategoryId=K"
+
+  let result = getHtmlTextinGS(url)
+  
+  const pattern = '<div class="end_date_price"><span class="fund_value">'
+  let startIndex = result.lastIndexOf(pattern) + pattern.length
+  let newString = result.substring(startIndex)
+  let endIndex = newString.indexOf(' <')
+  let priceString = newString.substr(0, endIndex).replace(',','.')
+  
+  let price = parseFloat(priceString)
+  
+  return price
+}
+
+function getHtmlText(url) {
+  let response = await fetch(url) // if CORS error, use chrome plugin
+  let htmlText = await response.text()
+
+  return htmlText
+}
+
+function getHtmlTextinGS(url) {
+  let options = {
+    "method": "GET",
+    "followRedirects": true,
+    "mode": "no-cors",
+    "muteHttpExceptions": true
   }
+
+  return UrlFetchApp.fetch(url, options).getContentText();
+}
