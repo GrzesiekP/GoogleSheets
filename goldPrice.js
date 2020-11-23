c45ae83704789a4cd576f18f36df55f93462ec9d
Grzesiek P <kontakt@grzegorzpawlowski.pl>
2020-11-23 19:23:56 +0100

    użyj const

--- goldPrice.js
+++ goldPrice.js
@@ -2,23 +2,23 @@ function GOLD_PRICE(currency) {
     // --> Sprawdź wyjaśnienie tego kodu na grzegorzpawlowski.pl <--
     // Tym razem do początkowej części url, doklejami symbol waluty, w której checmy uzyskać wynik
     // Jeśli jako currency podamy "PLN", dostaniemy url https://data-asg.goldprice.org/dbXRates/PLN
-    var url = "https://data-asg.goldprice.org/dbXRates/" + currency;
+    const url = "https://data-asg.goldprice.org/dbXRates/" + currency;
     
     // Opcje, które definiują, jak kod ma wykonać poprawne zapytanie.
-    var requestOptions = {
+    const requestOptions = {
         "method": "GET",
         "followRedirects": true,
         "muteHttpExceptions": true
     };
 
     // Wykonanie zapytania - odpowiedź w zmiennej result.
-    var result = UrlFetchApp.fetch(url, requestOptions);
+    const result = UrlFetchApp.fetch(url, requestOptions);
 
     // Odpowiedź jest w formacie JSON, więc trzeba zamienić go na objekt JavaScriptowy.
-    var parsedData = JSON.parse(result.getContentText());
+    const parsedData = JSON.parse(result.getContentText());
 
     // Na koniec z obiektu zawierającego całą odpowiedź, wyciągam to, co mnie interesuje - cenę złota.
-    var goldPrice = parsedData['items'][0]['xauPrice'];
+    const goldPrice = parsedData['items'][0]['xauPrice'];
 
     // I zwracam ją jako wynik funkcji
     return goldPrice;
--- usdGoldPrice.js
+++ usdGoldPrice.js
@@ -1,23 +1,23 @@
 function GOLD_PRICE() {
     // --> Sprawdź wyjaśnienie tego kodu na grzegorzpawlowski.pl <--
     // Link do danych. Spróbuj wkleić go w przeglądarce, żeby zobaczyć, co zwraca.
-    var url = "https://data-asg.goldprice.org/dbXRates/USD";
+    const url = "https://data-asg.goldprice.org/dbXRates/USD";
     
     // Opcje, które definiują, jak kod ma wykonać poprawne zapytanie.
-    var requestOptions = {
+    const requestOptions = {
         "method": "GET",
         "followRedirects": true,
         "muteHttpExceptions": true
     };
 
     // Wykonanie zapytania - odpowiedź w zmiennej result.
-    var result = UrlFetchApp.fetch(url, requestOptions);
+    const result = UrlFetchApp.fetch(url, requestOptions);
 
     // Odpowiedź jest w formacie JSON, więc trzeba zamienić go na objekt JavaScriptowy.
-    var parsedData = JSON.parse(result.getContentText());
+    const parsedData = JSON.parse(result.getContentText());
 
     // Na koniec z obiektu zawierającego całą odpowiedź, wyciągam to, co mnie interesuje - cenę złota.
-    var goldPrice = parsedData['items'][0]['xauPrice'];
+    const goldPrice = parsedData['items'][0]['xauPrice'];
 
     // I zwracam ją jako wynik funkcji
     return goldPrice;
